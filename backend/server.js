ae0544d3c40a7a18fc3190afea79c5404fdf5a84
Glenn <glennoliva122@gmail.com>
2024-11-23 22:57:08 +0800

    new commit!

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const db = require('../backend/dbcon');  // Import the db connection
+const db = require('./dbcon');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const path = require('path');
